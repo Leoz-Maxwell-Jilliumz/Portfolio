77f240df31127c91f37ecbbdd8a7bc602a7269db
Jamin Workman <jaminworkman@Jamins-MacBook-Air.local>
2026-03-06 17:02:09 -0500

    removed unused elements

--- portfolio-1/src/App.tsx
+++ portfolio-1/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Routes, Route, Router} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Homepage from './pages/Homepage'
 import Navbar from './pages/Navbar';
