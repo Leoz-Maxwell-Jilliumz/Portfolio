b7901d0a759900ed70c639da7e23cc6e2cc7b68c
Jamin Workman <jaminworkman@Jamins-MacBook-Air.local>
2026-03-07 17:00:41 -0500

    punctuation change

--- portfolio-1/src/main.tsx
+++ portfolio-1/src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename='/Portfolio/'>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
--- portfolio-1/src/pages/About.tsx
+++ portfolio-1/src/pages/About.tsx
@@ -5,7 +5,7 @@ function About(){
             <img src="./Jamin-Bust.jpg" alt="Jamin Workman" className="border-none rounded-xl w-100 h-100 md:w-100 md:h-100 mb-4 md:ml-12 md:mb-0 mt-8 md:mt-0" />
             <div className="w-full md:w-1/2 flex flex-col justify-center items-center gap-6 m-0 p-0 md:mr-16">
                 <h1 className="text-white text-4xl text-center md:text-6xl">About Me</h1>
-                <p className="text-white text-md md:text-lg text-center">I’m a software developer passionate about building practical tools and applications that solve real problems. My primary stack includes JavaScript, TypeScript, React, and modern backend technologies. I enjoy the entire development process—from designing clean user interfaces to building efficient backend systems and databases. I’m always learning new technologies and looking for opportunities to work on meaningful products with strong teams.</p>
+                <p className="text-white text-md md:text-lg text-center">I’m a software developer passionate about building practical tools and applications that solve real problems. My primary stack includes JavaScript, TypeScript, React, and modern backend technologies. I enjoy the entire development process—from designing clean user interfaces to building efficient backend systems and databases. I’m always learning new technologies and looking for opportunities to work on meaningful products with strong teams!</p>
             </div>
         </div>
     )
